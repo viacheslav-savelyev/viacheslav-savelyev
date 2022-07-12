8e93ecf80c1d08f4c84d2c00a73a49d02b3e7427
Viacheslav Savelyev <109039391+viacheslav-savelyev@users.noreply.github.com>
2022-07-12 20:59:27 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const hamburger = document.querySelector('.hamburger'),
+		menu = document.querySelector('.menu'),
+		closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+	menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+	menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+		lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+	lines[i].style.width = item.innerHTML;
+});
+
+// Page up
+
+$(document).scroll(function () {
+	if ($(this).scrollTop() > 600) {
+		$('.pageup').fadeIn();
+	} else {
+		$('.pageup').fadeOut();
+	}
+});
+
